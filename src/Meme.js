9cbe64d3b2bd09a1279028259a4ae8910266eedd
jindudimuna <danieldimuna@gmail.com>
2022-05-31 11:44:21 +0100

    state updates

--- src/Meme.js
+++ src/Meme.js
@@ -2,15 +2,26 @@ import React from "react";
 import memeData from "./memeData.js";
 
 function Form(){
-const [memeImage, setMemeImage] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memeData)
 
 
-       
+   
     function getMemeImage() {
-        const memesArray = memeData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
-    }   
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
+    }
+    
     return(
         <div className="main">
             <form className="meme-body">
@@ -21,7 +32,7 @@ const [memeImage, setMemeImage] = React.useState("")
               
             <button className="meme-button" onClick={getMemeImage}>Get a new meme image  🖼</button>
 
-            <img src={memeImage} className="meme-image" alt="jokes"/>
+            <img src={meme.randomImage} className="meme-image" alt="jokes"/>
 
         </div>
 
